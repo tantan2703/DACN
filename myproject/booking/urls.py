5cc0fc141b0cd4a2b9f873cf660a41a4965e571a
buichibaoltt <buichibaoltt@gmail.com>
2024-01-18 11:16:09 +0700

    update booking

--- myproject/booking/urls.py
+++ myproject/booking/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import concert_detail
-from .views import concert_booking_info
-
+from .views import concert_detail, concert_booking_info, payment
+from django.conf import settings
 urlpatterns = [
     path('concert/<str:name>/', concert_detail, name='concert_detail'),
     path('concert/<str:name>/booking/', concert_booking_info, name='concert_booking_info'),
+    path('concert/<str:name>/payment/', payment, name='payment')
 ]
 
--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -126,6 +126,10 @@ USE_I18N = True
 
 USE_TZ = True
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+QR_CODES_DIR = 'qr_codes'
+
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR,'static')
